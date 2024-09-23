d4fbbac450fc5249db0dcee23f1fd6e1a6fb93a5
seungyun-Park <tmdrbs0925@gmail.com>
2024-09-23 15:37:24 +0900

    1890

---
+++ silver/1890.js
@@ -0,0 +1,32 @@
+function solution(N, input, DP) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (DP[i][j] === 0 || ((i === N - 1) && (j === N - 1))) 
+            continue;
+        else {
+          let jump = input[i][j];
+          let down = i + jump;
+          let right = j + jump;
+  
+          if (down < N) DP[down][j] += BigInt(DP[i][j]);
+          if (right < N) DP[i][right] += BigInt(DP[i][j]);
+        }
+      }
+    }
+  }
+  
+  function start() {
+    const fs = require('fs').readFileSync('./input.txt');
+    let input = fs.toString().trim().split('\n');
+  
+    const N = Number(input.shift());
+    input = input.map(x => x.split(' ').map(x => Number(x)));
+    let DP = new Array(N).fill(null).map(x=>new Array(N).fill(BigInt(0)));
+    DP[0][0] = 1;
+  
+    solution(N, input, DP);
+    console.log(DP);
+    console.log(DP[N - 1][N - 1].toString());
+  }
+  
+  start();
